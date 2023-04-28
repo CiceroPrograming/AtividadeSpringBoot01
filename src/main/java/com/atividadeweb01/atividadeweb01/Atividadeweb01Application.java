75011f9f681324e6194d9ab19a11c51dfd4a9e88
Your Name <guest01@alunos.sc.senac.br>
2023-04-27 21:16:15 -0300

    primeiro commit

---
+++ src/main/java/com/atividadeweb01/atividadeweb01/Atividadeweb01Application.java
@@ -0,0 +1,13 @@
+package com.atividadeweb01.atividadeweb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividadeweb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividadeweb01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividadeweb01/atividadeweb01/Atividadeweb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividadeweb01.atividadeweb01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividadeweb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
